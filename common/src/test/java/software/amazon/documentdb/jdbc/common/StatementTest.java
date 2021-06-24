cba48b8412c188346ee6207c49ca83a3ca447a58
mitchell-elholm <76969722+mitchell-elholm@users.noreply.github.com>
2021-06-24 13:30:10 -0700

    Fixed Statement test

--- common/src/test/java/software/amazon/documentdb/jdbc/common/StatementTest.java
+++ common/src/test/java/software/amazon/documentdb/jdbc/common/StatementTest.java
@@ -47,7 +47,7 @@ public class StatementTest {
         HelperFunctions.expectFunctionDoesntThrow(() -> statement.setFetchDirection(ResultSet.FETCH_FORWARD));
         HelperFunctions.expectFunctionDoesntThrow(() -> statement.setEscapeProcessing(false));
         HelperFunctions.expectFunctionDoesntThrow(() -> statement.setFetchSize(0));
-        HelperFunctions.expectFunctionDoesntThrow(() -> statement.setFetchSize(1));
+        HelperFunctions.expectFunctionThrows(() -> statement.setFetchSize(-1));
         HelperFunctions.expectFunctionThrows(() -> statement.setLargeMaxRows(-1));
         HelperFunctions.expectFunctionDoesntThrow(() -> statement.setLargeMaxRows(1));
         HelperFunctions.expectFunctionThrows(() -> statement.setMaxFieldSize(-1));
