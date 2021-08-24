3755506ccbc018256fd499a70d8af8ace6a6e709
Bruce Irschick <brucei@bitquilltech.com>
2021-08-23 20:27:59 -0700

    [AD-308] Fix merge error. https://bitquill.atlassian.net/browse/AD-308

--- calcite-adapter/src/main/java/software/amazon/documentdb/jdbc/calcite/adapter/DocumentDbJoin.java
+++ calcite-adapter/src/main/java/software/amazon/documentdb/jdbc/calcite/adapter/DocumentDbJoin.java
@@ -55,7 +55,6 @@ import software.amazon.documentdb.jdbc.metadata.DocumentDbMetadataTable;
 import software.amazon.documentdb.jdbc.metadata.DocumentDbSchemaColumn;
 import software.amazon.documentdb.jdbc.metadata.DocumentDbSchemaTable;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
