439390e3a6a8a32d27feb40e06737ba08dce4b1c
Bruce Irschick <brucei@bitquilltech.com>
2021-12-10 14:32:12 -0800

    Revert incorrect merge.

--- calcite-adapter/src/test/java/software/amazon/documentdb/jdbc/metadata/DocumentDbMetadataScannerTest.java
+++ calcite-adapter/src/test/java/software/amazon/documentdb/jdbc/metadata/DocumentDbMetadataScannerTest.java
@@ -22,8 +22,6 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.BsonDocument;
 import org.bson.BsonInt32;
 import org.bson.BsonObjectId;
-import org.bson.ByteBuf;
-import org.bson.RawBsonDocument;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -93,7 +91,7 @@ public class DocumentDbMetadataScannerTest extends DocumentDbFlapDoodleTest {
         final MongoCollection<BsonDocument> collection = database.getCollection("testGetIteratorBasic",
                 BsonDocument.class);
 
-        final Iterator<BsonDocument> iterator = collection.find().limit(1).iterator(); // DocumentDbMetadataScanner.getIterator(properties, collection);
+        final Iterator<BsonDocument> iterator = DocumentDbMetadataScanner.getIterator(properties, collection);
 
         Assertions.assertTrue(documentSet.contains(iterator.next()));
         Assertions.assertThrows(NoSuchElementException.class,
